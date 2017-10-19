315e8a001f714872f0d207afc7381fe3fa52b0c3
mzedlach <mariazlach@gmail.com>
2017-10-19 11:54:51 +0200

    Sitcky Nav + Scrolling Nav

---
+++ resources/js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    
+    
+//    Sticky Navigation
+    $('.js-aboutme').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        };
+
+    });
+    
+//    Scrolling Navigation
+    $('.js-scroll-to-aboutme').click(function () {
+        $('html, body').animate({scrollTop: $('#aboutme').offset().top}, 500);
+    });
+    $('.js-scroll-to-projects').click(function () {
+        $('html, body').animate({scrollTop: $('#projects').offset().top}, 500);
+    });
+    $('.js-scroll-to-contact').click(function () {
+        $('html, body').animate({scrollTop: $('#contactform').offset().top}, 500);
+    });
+    
+});
+
