7cc5b8a7b5401cec994cb84e3dd076d717aa9d26
mzedlach <mariazlach@gmail.com>
2018-12-03 11:38:42 +0100

    Event Listener for Navigation box to not display nav once location clicked

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 
 window.onscroll = function() {navAndAnimations()};
 
-
 // Get sticky navigation section
 var stickyNav = document.getElementById("stickyNavigation");
 // Get about section
 var about = document.getElementById("about");
 // Get the offset position of the About section
-var sticky = about.offsetTop;
+var sticky = about.offsetTop -10;
 
 //variables of div containing elements with animation. 
 var skillsList1Location = (document.getElementById("skills1")).offsetTop;
@@ -18,16 +17,18 @@ var projectTile3Location = (document.getElementById("projectTrigger3")).offsetTo
 var contactLocation = (document.getElementById("contactTriggerTitle")).offsetTop;
 
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navAndAnimations() {
 
   // ----- STICKY NAVIGATION ----- //
+  // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
   if (window.pageYOffset > sticky) {
     stickyNav.classList.add("sticky");
-    //Check ES9 compatiblitiy fix on W3schools (how TO add a class)
+    //Check ES9 compatiblitiy fix on W3schools (how TO add a class)****
   } else {
     stickyNav.classList.remove("sticky");
   }
+
+
   //There has to be a more efficient way to do this. This is not very DRY :|
   // ----- ANIMATIONS ----- //
   var height = window.innerHeight
@@ -80,5 +81,16 @@ function navAndAnimations() {
   } else {
     contactTriggerTitle.classList.remove("animated","flipInY");
   }
-
 }
+
+// ----- CLICK EVENT LISTENER ----- //
+// To close navigation box once a location is clicked, for mobile. 
+// Thank you Chris Ferdanandi for the following tutorial: https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/ 
+document.addEventListener('click', function (event) {
+	// If the clicked elmeent is one of the nav elements, set checkbox to false. 
+	if (event.target.matches('#nav-About') || event.target.matches('#nav-Skills') || event.target.matches('#nav-Projects') || event.target.matches('#nav-Contact') ) {
+    document.getElementById("nav-toggle").checked = false;
+  };
+  // Otherwise, bail. 
+  return;
+}, false);
